80d1cc8a5636ef0c05eb2f71721052ce8bd97fd1
Tabetha Ketzner <tabketzner@gmail.com>
2012-03-08 19:44:51 -0600

    Ready

--- gitHubExample/ViewController.h
+++ gitHubExample/ViewController.h
@@ -5,8 +5,9 @@
 //  Created by Tabetha Ketzner on 3/8/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
-
+//Here is a handy comment
 #import <UIKit/UIKit.h>
+
 
 @interface ViewController : UIViewController
 
